905456d555068a65dfe8171512a04f6cb32e275e
Rafael Silva <rs7951006@gmail.com>
2022-10-26 09:32:04 -0300

    Add files via upload

---
+++ array/01_.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+arr.forEach(function(el, index) {
+    console.log(`Posição ${index}: valor ${el}`)
+})
+
+console.log("___________________________________________")
+
+let sum = arr[4] + arr[8]
+console.log(sum)
