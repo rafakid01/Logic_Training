4c00588aa9592a048aff4e02b6ef894cce663ee2
Rafael Silva <rs7951006@gmail.com>
2022-10-31 08:14:23 -0300

    point

--- array/01_.js
+++ array/01_.js
@@ -1,10 +1,10 @@
-let arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-arr.forEach(function(el, index) {
-    console.log(`Posição ${index}: valor ${el}`)
-})
+arr.forEach(function (el, index) {
+  console.log(`Posição ${index}: valor ${el}`);
+});
 
-console.log("___________________________________________")
+console.log("___________________________________________");
 
-let sum = arr[4] + arr[8]
-console.log(sum)
+let sum = arr[4] + arr[8];
+console.log(sum);
